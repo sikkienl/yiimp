841b8deba19d6e240c71796b66a04837e0449b15
Decho Kocharin <sourcedecho@outlook.com>
2025-11-13 11:55:50 +0700

    Update equihash algorithms insert statements

--- sql/2024-04-22-add_equihash_algos.sql
+++ sql/2024-04-22-add_equihash_algos.sql
@@ -1,5 +1,5 @@
-INSERT INTO `yiimp`.`algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash', 1, '#d0b0d0', 2142, 1);
-INSERT INTO `yiimp`.`algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash125', 1, '#d0b0d0', 2150, 0.001);
-INSERT INTO `yiimp`.`algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash144', 1, '#d0b0d0', 2146, 0.001);
-INSERT INTO `yiimp`.`algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash192', 1, '#d0b0d0', 2144, 0.001);
-INSERT INTO `yiimp`.`algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash96', 1, '#d0b0d0', 2148, 0.001);
+INSERT INTO `algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash', 1, '#d0b0d0', 2142, 1);
+INSERT INTO `algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash125', 1, '#d0b0d0', 2150, 0.001);
+INSERT INTO `algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash144', 1, '#d0b0d0', 2146, 0.001);
+INSERT INTO `algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash192', 1, '#d0b0d0', 2144, 0.001);
+INSERT INTO `algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('equihash96', 1, '#d0b0d0', 2148, 0.001);
