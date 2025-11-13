cd160f3a09ef15f35abb1efbb83a2360a6525404
Decho Kocharin <sourcedecho@outlook.com>
2025-11-13 11:57:22 +0700

    Fix SQL insert statement for neoscrypt-xaya algorithm

--- sql/2024-05-04-add_neoscrypt_xaya_algo.sql
+++ sql/2024-05-04-add_neoscrypt_xaya_algo.sql
@@ -1,1 +1,1 @@
-INSERT INTO `yiimp`.`algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('neoscrypt-xaya', 1, '#a0d0f0', 4238, 1);
+INSERT INTO `algos` (`name`, `factor`, `color`, `port`, `visible`) VALUES ('neoscrypt-xaya', 1, '#a0d0f0', 4238, 1);
